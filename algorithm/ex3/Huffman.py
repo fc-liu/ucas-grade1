e6b13f5de01e9d97ad0a6c01f1d7aa29cd8b2a9b
lfc <1635066070@qq,com>
2016-10-27 23:28:22 +0800

    finish huffman-code

--- algorithm/ex3/Huffman.py
+++ algorithm/ex3/Huffman.py
@@ -16,12 +16,16 @@ class Huffman_encode:
         self.__freq_word = []
         self.tree = Node(None, 0)
         self.encode_dic = {}
+        self.append_key = "appends"
+        self.end_flag = "end\n"
 
     def encode(self, input_file, output_file_path):
         self.__freq_word = self.char_freq(input_file)
+        print(self.__freq_word)
         hfm_tree = Huffman_tree()
         self.tree = hfm_tree.build_tree_from_freq(self.__freq_word)
         self.encode_dic = hfm_tree.code_from_tree(self.tree)
+        print(self.encode_dic)
         total_len_bin = 0
 
         for c in self.encode_dic:
@@ -30,58 +34,64 @@ class Huffman_encode:
         add_bits = 8 - total_len_bin % 8
 
         # write encode information to the file head
-        output_file = open(output_file_path, "c")
+        output_file = open(output_file_path, "w")
         for c in self.encode_dic:
             code_str = c + ":" + self.encode_dic[c] + "\n"
             output_file.write(code_str)
-        output_file.write("appends:" + str(add_bits))
+        output_file.write((self.append_key + ":" + str(add_bits) + "\n"))
+        output_file.write(self.end_flag)
         output_file.close()
 
         try:
             write = ""
-            in_file = open(input_file, 'rb')
-            chunk = in_file.read(1)
+            in_file = open(input_file, 'r')
+            chunk = in_file.readline()
             while chunk:
-                char = struct.unpack('c', bytes(chunk))[0]
-                # print(char)
-                code = self.encode_dic[char]
-                write = write + code
-
-                chunk = in_file.read(1)
+                for char in chunk:
+                    # char = struct.unpack('c', bytes(chunk))[0]
+                    # print(char)
+                    code = self.encode_dic[char]
+                    write = write + code
+                    buffer_size = 256
+                    if len(write) > buffer_size:
+                        self.write_file(write[:buffer_size], output_file_path)
+                        write = write[buffer_size:]
+                chunk = in_file.readline()
+            self.write_file(write, output_file_path)
         finally:
             in_file.close()
         return
 
     def write_file(self, str_code, output_file_path):
         try:
-            out_file = open(output_file_path, 'wa')
+            out_file = open(output_file_path, 'ab')
             l = len(str_code)
             if l % 8 != 0:
                 n = 8 - l % 8
                 str_add = "0" * n
                 str_code = str_code + str_add
-            while len(str_code) > 8:
+            while len(str_code) >= 8:
                 str_write = str_code[0:8]
                 str_code = str_code[8:]
-                code_write = bytes(int(str_write, 2))
-                out_file.write(code_write)
+                code_write = int(str_write, 2)
+                out_file.write(struct.pack('B', code_write))
         finally:
             out_file.close()
 
     def char_freq(self, input_file):
-        encode_file = open(input_file, 'rb')
+        encode_file = open(input_file, 'r')
         try:
-            chunk = encode_file.read(1)
+            chunk = encode_file.readline()
             while chunk:
-                char = struct.unpack('c', bytes(chunk))[0]
-                # print(char)
-                if char in self.encode_dic:
-                    self.encode_dic[char] = self.encode_dic[char] + 1
-                else:
-                    self.encode_dic[char] = 1
-                chunk = encode_file.read(1)
+                for char in chunk:
+                    if char in self.encode_dic:
+                        self.encode_dic[char] = self.encode_dic[char] + 1
+                    else:
+                        self.encode_dic[char] = 1
+                chunk = encode_file.readline()
         finally:
             encode_file.close()
+        print(self.encode_dic)
         return self.encode_dic
 
 
@@ -111,12 +121,27 @@ class Huffman_tree:
         return self.nodes[0]
 
     def build_tree_from_code(self, code_dic):
-        return
+        root = Node(None, 0)
+        for code_key in code_dic:
+            temp = root
+            code = code_dic[code_key]
+            for c in code:
+                if c == '0':
+                    if temp.left_node is None:
+                        temp.left_node = Node(None, 0)
+                    temp = temp.left_node
+                else:
+                    if temp.right_node is None:
+                        temp.right_node = Node(None, 0)
+                    temp = temp.right_node
+            temp.char = code_key
+        print("build tree from code finish!!!!!")
+        return root
 
     def code_from_tree(self, root):
         dic = {}
         if root.char is not None:
-            return {str(root.char): ""}
+            return {root.char: ""}
         else:
             dic_left = self.code_from_tree(root.left_node)
             for c in dic_left:
@@ -133,56 +158,111 @@ class Huffman_tree:
 
 
 class Huffman_decode:
-    def __init__(self, encode_dic):
-        self.dic = encode_dic
+    def __init__(self):
+        self.dic = {}
+        self.append_key = "appends"
+        self.end_flag = "end"
+        self.root = None
+
+    def decode(self, input_file_path, output_file_path):
+        print("start decode!!!!!")
+        encode_file = open(input_file_path, 'rb')
+        self.dic = self.get_encode_dic(encode_file)
+        appends_count = self.dic[self.append_key]
+        del self.dic[self.append_key]
+        ori_text = ""
+
+        hfm_tree = Huffman_tree()
+        self.root = hfm_tree.build_tree_from_code(self.dic)
+        chunk = encode_file.read(1)
+        temp_node = self.root
+        while chunk:
+            integer = struct.unpack('B', bytes(chunk))[0]
+            bin_str = self.__int2binStr(integer)
+            chunk = encode_file.read(1)
+            if chunk is None:
+                bin_str = bin_str[:8 - appends_count]
+
+            c = ''
+
+            for sc in bin_str:
+                if temp_node.char:
+                    c = temp_node.char
+                    temp_node = self.root
+                    # print(str(c))
+                    ori_text += c
+                if sc == '0':
+                    temp_node = temp_node.left_node
+                else:
+                    temp_node = temp_node.right_node
+
+        decode_file = open(output_file_path, 'w')
+        decode_file.write(ori_text)
+
+    def get_encode_dic(self, file):
+        encode_dic = {}
+        str_line = file.readline()
+        str_line = str_line.decode('ascii')[:-1]
+        while str_line != self.end_flag:
+            # print(str_line)
+            par = str_line.split(':')
+            if len(par) == 3:
+                par[0] = ':'
+                par[1] = par[2]
+            if len(par) < 2:
+                str_line = file.readline().decode('ascii')[:-1]
+                par = str_line.split(":")
+                par[0] = "\r\n"
+            # print(par[1])
+            if par[0] not in encode_dic:
+                encode_dic[par[0]] = par[1]
+            str_line = file.readline().decode('ascii')[:-1]
+        print("finish get encode dic!!!")
+        return encode_dic
+
+    def __int2binStr(self, number):
+        s = bin(number).replace('0b', '')
+        s = "0" * (8 - len(s)) + s
+        return s
 
 
 if __name__ == '__main__':
-    # n1 = Node(10)
-    # n2 = Node(12)
-    # n3 = Node(1)
-    # n4 = Node(2)
-    # n5 = Node(3432)
-    # n6 = Node(25)
-    # n7 = Node(6)
-    # n8 = Node(42)
-    #
-    # nl = [n1, n2, n3, n4, n5, n6, n7, n8]
-    # hfm = Huffman_tree()
-    # file_path = "Aesop_Fables.txt"
-    # hfm_encode = Huffman_encode()
-    # freq = hfm_encode.char_freq(file_path)
-    # nn = sorted(freq.items(), key=lambda d: d[1], reverse=True)
-    # # for n in nn:
-    # #     print(n.val)
-    # root = hfm.build_tree_from_freq(freq)
-    # print(nn)
-    #
-    # code = hfm.code_from_tree(root)
-    # print(code)
+    input_1_file = "Aesop_Fables.txt"
+    encode_1_file = "encode_Ae.txt"
+    decode_1_file = "decode_As.txt"
+    input_2_file = "graph.txt"
+    encode_2_file = "encode_g.txt"
+    decode_2_file = "decode_g.txt"
 
-    # print("finish")
-    # print("hahah")
+    hfm_encode = Huffman_encode()
+    hfm_decode = Huffman_decode()
+    hfm_encode.encode(input_1_file, encode_1_file)
+    hfm_decode.decode(encode_1_file, decode_1_file)
 
-    file_test = "test.txt"
-    file = open(file_test, 'a+')
-    str1 = "01010101"
-    str2 = "10010011"
-    code = str(chr(int(str1, 2)))
-    print(code)
-    file.write(code)
-    code = str(chr(int(str2, 2)))
-    print(code)
-    file.write(code)
-    file.close()
+    hfm_encode = Huffman_encode()
+    hfm_decode = Huffman_decode()
+    hfm_encode.encode(input_2_file, encode_2_file)
+    hfm_decode.decode(encode_2_file, decode_2_file)
 
-    input = open(file_test, 'rb')
-    chunk = input.read(1)
-    char = struct.unpack('c', bytes(chunk))[0]
-    s = str(char)
-    print(s)
 
-    chunk = input.read(1)
-    char = struct.unpack('c', bytes(chunk))[0]
-    s = str(char)
-    print(s)
+    # file_path = "test.txt"
+    # file_write = open("out.txt", "wb")
+    # s = "few\n"
+    # s = s.encode('ascii')
+    # print(s)
+    # file_write.write(s)
+    # s = "cdsav"
+    # s = s.encode('ascii')
+    # file_write.write(s)
+    # file = open(file_path, "rb")
+    # line = file.readline()
+    # while line:
+    #     print(line)
+    #     s = line.decode('ascii')
+    #     # print(s)
+    #     # print(type(s))
+    #     # print(a)
+    #     print(str(s))
+    #     print((s).__len__())
+    #     file_write.write(line)
+    #     line = file.readline()
